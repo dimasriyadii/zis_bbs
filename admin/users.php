68630d4548b6725c4cbfc95386059231abd9151c
dimasriyadii <dimasriyadi99@gmail.com>
2020-03-02 22:18:39 +0700

    remove meta tag

--- admin/users.php
+++ admin/users.php
@@ -6,9 +6,9 @@ include "../include/session.php";
 <html>
 <head>
 <title>User | Babussalam</title>
-<meta charset="utf-8">
+<!-- <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+<meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport"> -->
 
 
   <!-- jQuery 2.1.4 -->
