338b7cf20014fad1106e4ab0d29697cf2a1f7882
bellabeen <rootsquid@gmail.com>
2020-03-22 11:26:20 +0700

    Fix Report

--- admin/cetak.php
+++ admin/cetak.php
@@ -14,26 +14,7 @@ $pdf->AddPage();
 $pdf->SetFont('Arial','B',16);
 
 //mencetak string
-// $pdf->Image('kop.jpg',10,10,189);
-// $pdf->Cell(35,32,'',25,26);
-// $pdf->SetFont('Arial','B',12);
-// $pdf->Cell(190,7,'BUKTI PEMBAYARAN ZAKAT MASJID BABUSSALAM',0,1,'C');
-// $pdf->ln(7);
-// $pdf->SetFont('Arial','B',10);
-// $pdf->Cell(20,6,'Tanggal      : ',0,0);
-// $pdf->ln(10);
-// $pdf->Cell(40,6,'Atas Nama : ',0,0);
-// $pdf->ln(10);
-// $pdf->Cell(27,6,'Alamat        : ',0,0);
-// $pdf->ln(10);
-// $pdf->Cell(25,6,'Uang           : ',0,0);
-// $pdf->ln(10);
-// $pdf->Cell(30,6,'Beras          : ',0,0);
-// $pdf->ln(20);
-// $pdf->Cell(190,6,'PANITIA AMIL ZAKAT',0,1,'R');
-// $pdf->ln(20);
-// $pdf->Cell(190,6,'___________________',0,1,'R');
-
+$pdf->Image('kop.jpg',10,10,189);
 
 
 // Memberikan space kebawah agar tidak terlalu rapat
@@ -54,15 +35,15 @@ while ($zakat = mysqli_fetch_array($queryzakat)){
     $pdf->Cell(20,6,$zakat['tanggal']);
 
     $pdf->ln(10);
-    $pdf->Cell(40,6,'Atas Nama :  ',0,0);
+    $pdf->Cell(40,6,'Atas Nama :',0,0);
     $pdf->Cell(40,6,$zakat['nama']);
 
     $pdf->ln(10);
-    $pdf->Cell(27,6,'Alamat        : ',0,0);
+    $pdf->Cell(27,6,'Alamat        :');
     $pdf->Cell(27,6,$zakat['alamat'],0,0);
     
     $pdf->ln(10);
-    $pdf->Cell(25,6,'Uang           : ',0,0);
+    $pdf->Cell(25,6,'Uang           : ');
     $pdf->Cell(25,6,$zakat['uang'],0,0);
 
     $pdf->ln(10);
