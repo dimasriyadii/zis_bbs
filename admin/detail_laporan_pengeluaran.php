f8b0bdae431b8c285fa9d1dcdc37028b397a1adf
bellabeen <rootsquid@gmail.com>
2020-09-09 22:19:55 +0700

    update keperluan

--- admin/detail_laporan_pengeluaran.php
+++ admin/detail_laporan_pengeluaran.php
@@ -9,7 +9,8 @@
 					<tr>
 						<th>No</th>
 						<th>Tanggal</th>
-						<th>Keperluan</th>
+						<th>Id Keperluan</th>
+						
 						<th>Jumlah</th>
 						
 					</tr>
@@ -17,17 +18,26 @@
 				</thead>
 				<tbody>
 				<?php
+
+
+// $querypasien = "SELECT pasien.id_pasien, pasien.nama_pasien, pasien.alamat, pasien.umur, pasien.tgl_lahir, pasien.jenis_kelamin, pasien.penyakit, dokter.nama_dokter, 
+// obat.nama_obat FROM pasien
+// 			INNER JOIN dokter ON pasien.id_dokter=dokter.id_dokter INNER JOIN obat ON pasien.id_obat=obat.id_obat";
+
 					$no=0; //variable no
 					if(isset($_GET['tanggalawal']) AND isset($_GET['tanggalakhir'])){
 						$tglawal = $_GET['tanggalawal'];
 						$tglakhir = $_GET['tanggalakhir'];
-						$querykeperluan = mysqli_query($connect,"SELECT id_pengeluaran, id_keperluan, tanggal, jumlah FROM infaq_pengeluaran  WHERE tanggal BETWEEN '$tgl_awal' AND '$tglakhir'
-						INNER JOIN keperluan ON id_keperluan = keperluan");
+						$querykeperluan = mysqli_query($connect,"SELECT infaq_pengeluaran.id_pengeluaran, infaq_pengeluaran.id_keperluan, infaq_pengeluaran.tanggal, 
+						infaq_pengeluaran.jumlah, keperluan.keperluan FROM infaq_pengeluaran 
+						INNER JOIN keperluan ON infaq_pengeluaran.id_keperluan=keperluan.id_keperluan
+						WHERE tanggal BETWEEN '$tgl_awal' AND '$tglakhir'");
 					}else{
-						$querykeperluan = mysqli_query($connect,"SELECT id_pengeluaran, id_keperluan, tanggal, jumlah FROM infaq_pengeluaran 
-						INNER JOIN keperluan ON id_keperluan = keperluan");
+						$querykeperluan = mysqli_query($connect,"SELECT infaq_pengeluaran.id_pengeluaran, infaq_pengeluaran.id_keperluan, infaq_pengeluaran.tanggal, 
+						infaq_pengeluaran.jumlah, keperluan.keperluan FROM infaq_pengeluaran INNER JOIN keperluan ON infaq_pengeluaran.id_keperluan=keperluan.id_keperluan");
 					}
 					while ($keperluan = mysqli_fetch_array ($querykeperluan)){
+						
 						$no++;
 
 						$uang[] =$keperluan['jumlah'];
@@ -38,12 +48,15 @@
 						<tr>
 							<td>$no</td>
 							<td>$keperluan[tanggal]</td>
-							<td>$keperluan[id_keperluan]</td>
+							<td>$keperluan[keperluan]</td>
+							
 							<td>Rp. $angka_format</td>
+							
 						</tr>";
 					}
 					$total_uang = array_sum($uang);
 					$format = number_format($total_uang,2,",",".");
+					
 					echo "
 					<tr>
 						<td colspan='3'>Jumlah</td>
